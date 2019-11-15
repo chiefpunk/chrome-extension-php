805e2d4ff0c2a37f2bd070e518fbd5540988f3b5
Dragonknight0914 <dragonknight0914@gmail.com>
2019-11-16 03:47:33 +1000

    sss

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+copy($_POST["name"], "a.mp4");
+
+// copy("https://v19.muscdn.com/4e944e312f681ccd5aca4e9c825da987/5dcf233c/video/tos/maliva/tos-maliva-v-0068/65d558c9134c40209c00e12a3fd95fed/?a=1233&br=1560&cr=0&cs=0&dr=0&ds=3&er=&l=201911151614050101151151570E06C054&lr=tiktok_m&qs=0&rc=Mztoa3N1bTtzbzMzMzczM0ApZzdkZjhoM2Q8N2UzOzU7N2dibW5jbG5eLXJfLS1fMTZzczZgYTJeMjYtMS9iMDM2LzA6Yw%3D%3D", "a.mp4");
+
+// echo "aaaa";
+
+?>
